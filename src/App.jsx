b6ed378a17fbb5eb805b09b8c026e17a9c37cba5
Dextrodark <faizalmohamed5302002@gmail.com>
2023-08-25 16:13:03 +0530

    Updated Coded

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,9 @@ import { Hero,
 
 
 const App =() =>(
-  <main className="relative">
+  <main className="relative ">
     <Nav />
-    <section className="xl:padding-1 wide:padding-r padding-b">
+    <section className="xl:padding-l wide:padding-r padding-b">
       <Hero />
     </section>
     <section className="padding"><PopularProducts /></section>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -7,14 +7,14 @@ import {headerLogo} from '../assets/images';
     const Nav = () => {
     return (
         <header className='padding-x py-8 absolute z-10 w-full'  >
-            <nav className='flex justify-between items-center max-container'>
+            <nav className='flex justify-between  items-center max-container'>
                 <a href="/">
                     <img src={headerLogo} alt="Logo" width={130} height={29} />
                 </a>
-                <ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden'>
+                <ul className='flex-1 flex justify-center mr-10 items-center gap-16 max-lg:hidden'>
                     {navLinks.map((item) =>(
                         <li key={item.label}>
-                            <a href= {item.href} className='font-montserrat leading-normal text-lg text-slate-gray'>
+                            <a href= {item.href} className={`text-${item.label === 'Home' ? 'coral-red' : 'white'} hover:text-${item.label === 'Home' ? 'black' : 'red-500'} font-montserrat leading-normal text-lg`}>
                                 {item.label}
                             </a>
                         </li>
