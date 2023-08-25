764e25a81d30e5292006144bbb98de20cf96d6fb
Dextrodark <faizalmohamed5302002@gmail.com>
2023-08-25 17:13:49 +0530

    Added trending products

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -30,8 +30,8 @@
                 </div>
             </div>
 
-            <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary shadow-xl bg-hero bg-cover bg-center">
-                <img src={bigShoeImg} alt="shoe image" width={490} height={400} className="relative bottom-12 left-[32px] " />
+            <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary shadow-xl bg-hero bg-cover  bg-center">
+                <img src={bigShoeImg} alt="shoe image" width={490} height={400} className="  relative bottom-12   " />
                 <div className="flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6">
                     {shoes.map((shoe)=>(
                         <div key={shoe}>
--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -1,8 +1,22 @@
+    import { products } from "../constants";
+    import PopularProductCard from "../components/PopularProductCard";
+
     const PopularProducts = () => {
     return (
-        <div>
-        PopularProducts
-        </div>
+        <section id="products" className="max-container max-sm:mt-12 ">
+            <div className="flex flex-col justify-start overflow-x-hidden gap-5">
+                <h2 className="text-4xl font-palanquin font-bold">Our <span className="text-coral-red">Trending</span> <span>Products</span></h2>
+                <p className="text-xl lg:max-w-lg mt-2 font-montserrat text-slate-gray">
+                    Discover the latest and most sought-after trends in the world of shopping with our curated collection of trending products. Our website brings you an exciting array of items that are currently making waves in the market. 
+                </p>
+            </div>
+
+            <div className="mt-16 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 sm:gap-6 gap-14">
+                {products.map((product) => (
+                    <PopularProductCard key={product.name} {...product} />
+                ))}
+            </div>
+        </section>
     )
     }
 
